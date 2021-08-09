2e7d63cb8a6ac0bcff2a24f7d164336614a5d035
VatosLocoos <85027526+VatosLocoos@users.noreply.github.com>
2021-08-10 00:40:24 +0100

    Create AlignementNW.java

---
+++ src/outils/AlignementNW.java
@@ -0,0 +1,12 @@
+package outils;
+
+public class AlignementNW extends Sequence {
+	public AlignementNW(String s, String n, String t) {
+		super(s, n, t);
+		// TODO Auto-generated constructor stub
+	}
+
+	private int [][] matriceSequence;
+	
+	
+}
